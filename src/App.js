e3884be9c5bd6f8c747ed8920cc776dd19e427d0
scottdomes <scottdomes@gmail.com>
2017-04-21 11:40:48 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Router, browserHistory, Route, Link } from 'react-router'
+import logo from './logo.svg';
+import './App.css';
+
+const Page = ({ title }) => (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>{title}</h2>
+      </div>
+      <p className="App-intro">
+        This is the {title} page.
+      </p>
+      <p>
+        <Link to="/">Home</Link>
+      </p>
+      <p>
+        <Link to="/about">About</Link>
+      </p>
+      <p>
+        <Link to="/settings">Settings</Link>
+      </p>
+    </div>
+)
+
+const Home = (props) => (
+  <Page title="Home"/>
+)
+
+const About = (props) => (
+  <Page title="About"/>
+)
+
+const Settings = (props) => (
+  <Page title="Settings"/>
+)
+
+class App extends Component {
+  render() {
+    return (
+      <Router history={browserHistory}>
+        <Route path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/settings" component={Settings}/>
+      </Router>
+    );
+  }
+}
+
+export default App;
